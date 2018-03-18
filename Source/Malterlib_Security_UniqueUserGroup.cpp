02f05d44db35fdfa0889f11154d1e37e11df6c06
Erik Olofsson <erik@olofsson.info>
2018-03-18 17:16:41 +0100

    [Malterlib/Security] Add CUniqueUserGroup for unique user and group names across platforms

---
+++ Source/Malterlib_Security_UniqueUserGroup.cpp
@@ -0,0 +1,59 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include <Mib/Core/Core>
+#include <Mib/Cryptography/Hashes/SHA>
+
+#include "Malterlib_Security_UniqueUserGroup.h"
+
+namespace NMib::NSecurity
+{
+	using namespace NStr;
+
+	CUniqueUserGroup::CUniqueUserGroup(CStr const &_DefaultDirectory)
+	{
+		CStr ProgramDirectory = NFile::CFile::fs_GetProgramDirectory();
+		if (ProgramDirectory == _DefaultDirectory)
+		{
+			m_UserGroupNameTransform = "{}";
+			return;
+		}
+
+		NDataProcessing::CHash_SHA256 Hash;
+
+		CStr Salt = "MalterlibAppManagerUserGroupNameTransform";
+
+		Hash.f_AddData(Salt.f_GetStr(), Salt.f_GetLen());
+		Hash.f_AddData(_DefaultDirectory.f_GetStr(), _DefaultDirectory.f_GetLen());
+		Hash.f_AddData(ProgramDirectory.f_GetStr(), ProgramDirectory.f_GetLen());
+		m_UserGroupNameTransform = "{{}_{}"_f << Hash.f_GetDigest().f_GetString().f_Left(16);
+	}
+
+	CStr CUniqueUserGroup::f_TransformUserGroup(CStr const &_Name) const
+	{
+		if (_Name.f_IsEmpty())
+			return {};
+
+		return CStr::CFormat(m_UserGroupNameTransform) << _Name;
+	}
+
+	CStr CUniqueUserGroup::f_GetUser(CStr const &_Name) const
+	{
+		if (_Name.f_IsEmpty())
+			return {};
+
+		return NSys::fg_UserManagement_MakeValidUserName(f_TransformUserGroup(_Name));
+	}
+
+	CStr CUniqueUserGroup::f_GetGroup(CStr const &_Name) const
+	{
+		if (_Name.f_IsEmpty())
+			return {};
+
+#ifdef DPlatformFamily_Windows
+		return "Group_" + f_TransformUserGroup(_Name);
+#else
+		return f_TransformUserGroup(_Name);
+#endif
+	}
+}
---
+++ Source/Malterlib_Security_UniqueUserGroup.h
@@ -0,0 +1,22 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include <Mib/Core/Core>
+
+namespace NMib::NSecurity
+{
+	struct CUniqueUserGroup
+	{
+		CUniqueUserGroup(NStr::CStr const &_DefaultDirectory);
+
+		NStr::CStr f_GetUser(NStr::CStr const &_Name) const;
+		NStr::CStr f_GetGroup(NStr::CStr const &_Name) const;
+		NStr::CStr f_TransformUserGroup(NStr::CStr const &_Name) const;
+
+		NStr::CStr m_UserGroupNameTransform;
+	};
+}
+
+#ifndef DMibPNoShortCuts
+	using namespace NMib::NSecurity;
+#endif
